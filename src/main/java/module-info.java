6a6bcc8cd5c79ebd275747d9fdc13f6676b50ed1
geekbrains <Sea_1994@inbox.ru>
2022-05-03 14:26:26 +0700

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.example.chat_two {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.chat_two to javafx.fxml;
+    exports com.example.chat_two;
+}
